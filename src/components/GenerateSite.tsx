cd31b625c5f88a5b3943684875f158c340ca694c
qingyuano <211720950@qq.com>
2024-08-05 21:50:11 +0800

    update: add n-bookmark url

--- src/components/GenerateSite.tsx
+++ src/components/GenerateSite.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import React from 'react';
+import Link from 'next/link';
 import { Bookmark, convertToExcel } from '@/lib/parseBookmarks';
 
 import { Button } from './ui/button';
@@ -10,7 +11,11 @@ export default function GenerateSite(props: { data: Bookmark[] }) {
   return (
     <div className="z-10 flex w-full items-center justify-between rounded-lg border border-border bg-background/40 p-4 sm:w-96">
       <span>生成导航站</span>
-      <Button variant="link">计划中</Button>
+      <Button variant="link" asChild>
+        <Link href="https://github.com/QingYuanO/n-bookmark" target="_blank">
+          立即拥有
+        </Link>
+      </Button>
     </div>
   );
 }
